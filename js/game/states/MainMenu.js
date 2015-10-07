01b3756392426d3825fe61cfafc0d6e640bd0208
kellyhuang21 <kellyh21stuff@gmail.com>
2015-10-07 13:57:09 -0700

    wwww

---
+++ js/game/states/MainMenu.js
@@ -0,0 +1,14 @@
+Final-Stand-.MainMenun = function () {}
+tmplate.MainMenu.prototype = {
+    preload : function()
+     this.load.image ('whatever','assests/images/marla.png');
+},
+
+create: function(){
+}
+  this. pic = this. game .add. sprite (this.game.width/2, this.game.height/2 , 'whatever')
+};
+      }
+}
+
+
